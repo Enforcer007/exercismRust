9668c8d3db7fb4018df343cccd82004f909959a6
Akhil <akhilgodvsdemon@gmail.com>
2021-07-14 21:34:44 +0530

    Beer-Song

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,20 @@
+static VERSELAST:&str="No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+static VERSELASTBUTONE:&str="1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    if n == 0 {
+        return String::from(VERSELAST);
+    } else if n == 1 {
+        return String::from(VERSELASTBUTONE);
+    } else {
+        return format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle{} of beer on the wall.\n", n, n, n - 1, if n-1==1{""} else {"s"});
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let string = (end..=start)
+        .rev()
+        .map(|i| verse(i))
+        .fold(String::new(), |a, b| a + &b + "\n");
+    string[..string.len() - 1].into()
 }
