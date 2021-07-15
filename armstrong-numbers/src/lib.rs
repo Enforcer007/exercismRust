af08876bd09028b76cb6ddf0aecf41a8993eb34c
Akhil <akhilgodvsdemon@gmail.com>
2021-07-15 15:41:56 +0530

    A clear mathematical way

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,11 +1,7 @@
-use std::u32;
-
 pub fn is_armstrong_number(num: u32) -> bool {
-    let num_string = num.to_string();
-    let n_digits = num_string.len();
-    let arms_strong = num_string
-        .chars()
-        .map(|x| x.to_digit(10).unwrap_or(0).pow(n_digits as u32))
-        .sum::<u32>();
-    arms_strong == num
+    let digits = ((num as f64).log10() + 1.).floor() as u32;
+    (0..digits)
+        .map(|i| (num / 10u32.pow(i) % 10).pow(digits))
+        .sum::<u32>()
+        == num
 }
