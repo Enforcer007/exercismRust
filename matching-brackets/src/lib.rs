1dd41239cbe94cec0ecf099db68a50d35c71976f
Akhil <akhilgodvsdemon@gmail.com>
2021-07-16 05:58:51 +0530

    More clear way of Brackts Matching

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,26 +1,25 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let opening_braces: [char; 3] = ['{', '(', '['];
-    let closing_braces: [char; 3] = ['}', ')', ']'];
-    let mut buffer = Vec::<char>::new();
-    for i in string.chars() {
-        if opening_braces.contains(&i) {
-            buffer.push(i);
-        } else if closing_braces.contains(&i) {
-            let opposite_brace = match i {
-                '}' => '{',
-                ')' => '(',
-                _ => '[',
-            };
-            if let Some(x) = buffer.last() {
-                if opposite_brace == *x {
-                    buffer.pop();
-                } else {
+    let mut brackets: Vec<char> = Vec::new();
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => brackets.push(c),
+            ')' => {
+                if brackets.pop() != Some('(') {
                     return false;
                 }
-            } else {
-                return false;
             }
+            ']' => {
+                if brackets.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if brackets.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => (),
         }
     }
-    buffer.len() == 0
+    brackets.is_empty()
 }
