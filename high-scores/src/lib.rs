7e7811357c901dfbc5084305a3c377beee56f864
Akhil <akhilgodvsdemon@gmail.com>
2021-07-16 11:54:10 +0530

    High-Scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        Self {
+            scores: Vec::from(scores),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let n = self.scores.len();
+        let index = if n < 3 { n } else { 3 };
+        let mut s = self.scores.to_vec();
+        s.sort();
+        s.reverse();
+        Vec::from(&s[..index])
+    }
+}
